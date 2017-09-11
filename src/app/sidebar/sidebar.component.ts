329103c1a4e410b6c82ac13236847f3e3069fc5c
Anna Thomas <annaet@uk.ibm.com>
2017-09-11 16:41:57 +0100

    Style sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
-  constructor() { }
+  selected: any;
+
+  constructor() {
+    this.selected = 'overview';
+  }
+
+  select(route) {
+    this.selected = route;
+  }
 
 }
